02f32ba54efa2cf2313e0d9a05f63a0a01d746da
SraqZit <kingofmestry@gmail.com>
2022-03-12 19:21:11 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     description='A python module to download and upload from gigafile.nu',
     author='Sraqzit',
     author_email='kingofmestry@gmail.com',
+    install_requires=['requests==2.25.1', 'requests_toolbelt==0.9.1', 'tqdm==4.61.2'],
+    requires=[],
     packages=['gigafile'],
     entry_points={
         'console_scripts': [
