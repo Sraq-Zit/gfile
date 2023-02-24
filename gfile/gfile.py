d45fb674bc48102c3ad8bbfab2e4b3c7f326d93c
fireattack <human.peng@gmail.com>
2023-02-24 16:17:33 +0800

    Better progress bar

--- gfile/gfile.py
+++ gfile/gfile.py
@@ -85,9 +85,8 @@ class GFile:
         self.thread_num=thread_num
         self.progress = progress
         self.data = None
-        self.pbar: tqdm = None
+        self.pbar = None
         self.session = requests_retry_session()
-        self.index = 0
         self.cookies = None
         self.current_chunk = 0
 
@@ -106,6 +105,8 @@ class GFile:
         #     prev = current
 
         # memo('Before load')
+
+        bar = self.pbar[chunk_no % self.thread_num] if self.pbar else None
         with io.BytesIO() as f:
             split_file(self.uri, f, self.chunk_size, start=chunk_no * self.chunk_size, chunk_copy_size=self.chunk_copy_size)
             chunk_size = f.tell()
@@ -127,13 +128,21 @@ class GFile:
             del form_data
 
         size = len(form_data_binary)
+        if bar:
+            bar.desc = f'chunk {chunk_no + 1}/{chunks}'
+            bar.reset(total=size)
+            # bar.refresh()
 
         def gen():
             offset = 0
             while True:
                 if offset < size:
-                    yield form_data_binary[offset:offset+1024]
-                    offset += 1024
+                    update_tick = 1024 * 128
+                    yield form_data_binary[offset:offset+update_tick]
+                    if bar:
+                        bar.update(min(update_tick, size - offset))
+                        bar.refresh()
+                    offset += update_tick
                 else:
                     if chunk_no != self.current_chunk:
                         time.sleep(0.01)
@@ -147,9 +156,6 @@ class GFile:
         # print(f'Updating chunk {chunk_no + 1} out of {chunks} chunks')
         resp = self.session.post(f"https://{self.server}/upload_chunk.php", data=streamer, headers=headers)
         self.current_chunk += 1
-        if self.pbar:
-            self.pbar.desc = f'Finished {chunk_no + 1}/{chunks} chunks'
-            self.pbar.update(chunk_size)
         # print("Session gfsid after uploading:", self.session.cookies['gfsid'])
         # print('resp', resp.cookies.__dict__)
         resp_data = resp.json()
@@ -165,24 +171,24 @@ class GFile:
         self.token = uuid.uuid1().hex
         self.pbar = None
         self.failed = False
-        self.index = 0
         assert Path(self.uri).exists()
         size = Path(self.uri).stat().st_size
         chunks = ceil(size / self.chunk_size)
         print(f'Filesize {bytes_to_size_str(size)}, chunk size: {bytes_to_size_str(self.chunk_size)}, total chunks: {chunks}')
 
         if self.progress:
-            self.pbar = tqdm(total=size, unit="B", unit_scale=True, leave=False, unit_divisor=1024, ncols=100)
-
+            self.pbar = []
+            for i in range(self.thread_num):
+                self.pbar.append(tqdm(total=size, unit="B", unit_scale=True, leave=False, unit_divisor=1024, ncols=100, position=i))
         self.session = requests_retry_session()
         self.server = re.search(r'var server = "(.+?)"', self.session.get('https://gigafile.nu/').text)[1]
 
-        # upload the first chunk
+        # upload the first chunk to set cookies properly.
         self.upload_chunk(0, chunks)
 
         # upload second to second last chunk(s)
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.thread_num) as ex:
-            futures = {ex.submit(self.upload_chunk, i, chunks): i for i in range(1, chunks - 1)}
+            futures = {ex.submit(self.upload_chunk, i, chunks): i for i in range(1, chunks)}
             try:
                 for future in concurrent.futures.as_completed(futures):
                     if self.failed:
@@ -197,11 +203,14 @@ class GFile:
                 return
 
         # upload last chunk if not already
-        if chunks > 1:
-            # print('\nupload the last chunk in single thread')
-            self.upload_chunk(chunks - 1, chunks)
+        # if chunks > 1:
+        #     # print('\nupload the last chunk in single thread')
+        #     self.upload_chunk(chunks - 1, chunks)
 
-        if self.pbar: self.pbar.close()
+        if self.pbar:
+            for bar in self.pbar:
+                bar.close()
+        print('')
         if 'url' not in self.data:
             print('Something went wrong. Upload failed.', self.data)
         return self # for chain
@@ -228,14 +237,15 @@ class GFile:
             r.raise_for_status()
             filesize = int(r.headers['Content-Length'])
             if not filename:
+                filename = 'gigafile_noname.bin' # temp name
                 content_disp = r.headers['Content-Disposition']
                 if "UTF-8''" in content_disp:
                     filename = unquote(content_disp.split("UTF-8''")[-1])
                 else:
                     filename = re.search(r'filename="(.+?)";', content_disp)[1].encode('iso8859-1','ignore').decode('utf-8', 'ignore')
-                filename = re.sub(r'[\\/:*?"<>|]', '_', filename) # only sanitize remote filename. User provided are on users' own.
+                filename = re.sub(r'[\\/:*?"<>|]', '_', filename) # only sanitize remote filename. User provided ones are on users' own.
             if self.progress:
-                self.pbar = tqdm(total=filesize, unit='B', unit_scale=True, unit_divisor=1024, desc=filename, ncols=100)
+                self.pbar = tqdm(total=filesize, unit='B', unit_scale=True, unit_divisor=1024, desc=filename)
             with open(filename, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=self.chunk_copy_size):
                     f.write(chunk)
